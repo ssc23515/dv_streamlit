6561ed5022e95e6271575f031f2f18a51cfda671
ssc23515 <133722792+ssc23515@users.noreply.github.com>
2025-11-17 15:14:56 +0900

    Initialize Streamlit app with page configuration

---
+++ Streamlit_Lectures.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config( #페이지 설정
+ page_title="하교수의 Streamlit", #페이지 Tab의 타이틀
+ page_icon="", #페이지 Tab의 아이콘
+ layout="wide", #페이지 레이아웃: centered, wide
+ #사이드바 초기 상태 : auto, collapsed, expanded
+ initial_sidebar_state="expanded",
+ #페이지 오른쪽 상부의 메뉴에 추가할 메뉴 항목: Get help, Report a bug, About
+ menu_items={
+  'Get help': "https://docs.streamlit.io", # URL만
+  'Report a bug': "https://streamlit.io", #URL만
+  'About': "## 하정훈 교수"
+ }
+)
